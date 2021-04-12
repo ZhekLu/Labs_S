6bde0dbb8f96aa1ef3a6242d376c44eb78479a78
ZhekLu <73021444+ZhekLu@users.noreply.github.com>
2021-04-12 10:13:21 +0300

    ADD rlimit

--- LABA4/4_1.c
+++ LABA4/4_1.c
@@ -1,9 +1,18 @@
 #include "declensionOfNumber.h"
+#include <sys/resource.h>
+#include <sys/time.h>
+#include <unistd.h>
 /*#include <locale.h>*/
 
 int main()
 {
     /*setlocale(LC_ALL, "Rus");*/
+    
+    struct rlimit rl;
+    getrlimit(RLIMIT_CPU, &rl);
+    rl.rlim_cur = 1; 
+    setrlimit(RLIMIT_CPU, &rl);
+    
     int num, cases;
     GetParams(&num, &cases);
     char* number = NumberDeclension(num, HowManyDigits(num), cases);
