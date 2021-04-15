dd92c6a2517ef00ef3107086effbda65a8ab01a5
ZhekLu <73021444+ZhekLu@users.noreply.github.com>
2021-04-15 14:27:41 +0300

    Update 4_1.c

--- LABA4/4_1.c
+++ LABA4/4_1.c
@@ -5,6 +5,13 @@ int main()
 {
     /*setlocale(LC_ALL, "Rus");*/
     int num, cases;
+    
+    struct rlimit rl;
+    getrlimit(RLIMIT_CPU, &rl);
+    rl.rlim_cur = 3000000; 
+    /*setrlimit(RLIMIT_CPU, &rl);*/
+    setrlimit(RLIMIT_AS, &rl); 
+    
     GetParams(&num, &cases);
     char* number = NumberDeclension(num, HowManyDigits(num), cases);
     if(number)
