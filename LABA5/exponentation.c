dba01fa6ef5a923c23965dcb99b9ab94b5ea607e
ZhekLu <73021444+ZhekLu@users.noreply.github.com>
2021-04-15 18:23:43 +0300

    x..

--- LABA5/exponentation.c
+++ LABA5/exponentation.c
@@ -10,7 +10,7 @@ MultiNum CreateNum(char* str){
 
 void push_front(MultiNum* num, int digit){
     Item*x = (Item*)malloc(sizeof(Item)); 
-    if(!)
+    if(!X)
         return; 
     x->digit = digit; 
     x->ptrPrev = x->ptrNext = NULL;
