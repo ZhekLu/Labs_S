a16354a00d6ee209dda1db8ae37cd33180f30f1d
ZhekLu <73021444+ZhekLu@users.noreply.github.com>
2021-04-19 00:06:28 +0300

    rlimit_as

--- LABA4/4_1.c
+++ LABA4/4_1.c
@@ -1,23 +1,31 @@
 #include "declensionOfNumber.h"
+
 #include <sys/resource.h>
 #include <sys/time.h>
 #include <unistd.h>
-/*#include <locale.h>*/
+
 
 int main()
 {
-    /*setlocale(LC_ALL, "Rus");*/
-    
+   
     struct rlimit rl;
-    getrlimit(RLIMIT_CPU, &rl);
-    rl.rlim_cur = 1; 
-    setrlimit(RLIMIT_CPU, &rl);
+    rl.rlim_cur = 3000000;
+   
+    /* setrlimit(RLIMIT_CPU, &rl);*/
+    setrlimit(RLIMIT_AS, &rl); 
     
+    /*
+    // RLIMIT_RSS //ulimit -m //ps -aux // 
+    */
     int num, cases;
     GetParams(&num, &cases);
     char* number = NumberDeclension(num, HowManyDigits(num), cases);
     if(number)
         printf("|Result| %s\n", number);
+    else  
+        printf("\nThere's no enought memory for complete program.\n"); 
     free(number);
+
     return 0;
 }
+
