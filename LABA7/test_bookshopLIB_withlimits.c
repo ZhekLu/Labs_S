ca2d3f3e957e0044b8e13c4c90a8e562c136384b
ZhekLu <justletmego@list.ru>
2021-05-26 19:45:38 +0300

    Try to do smth with me mo ry

--- LABA7/test_bookshopLIB_withlimits.c
+++ LABA7/test_bookshopLIB_withlimits.c
@@ -8,7 +8,33 @@ int main()
 {
     NEWSHOP(sh);
     //----------------------Test function AddBooks()------------------------
+    
+    
+    //then...
+    struct rlimit lim, softlim;
+    getrlimit(RLIMIT_AS, &softlim);
+    getrlimit(RLIMIT_AS, &lim); 
+
+    lim.rlim_cur = 1000; 
+    setrlimit(RLIMIT_AS, &lim);
+    
+    //first malloc break
+    assert(AddBooks("booksfortest.txt", &sh) == 0);  
+    assert(sh.books == NULL); 
+    //restore..
+    setrlimit(RLIMIT_AS, &softlim);
+    assert(AddBooks("books.txt", &sh) == 1); 
+    assert(sh.books != NULL);
+    
+    // lim.rlim_cur = sizeof(Book*) * 2000 + 72; 
+    // setrlimit(RLIMIT_AS, &lim);
+    // //secmalloc
+    // assert(!AddBooks("booksfortest.txt", &sh)); 
+    // assert(sh.books == NULL);
+    // printf("LOL srabotalo"); 
+
     //part without rlimits//
+    
     //br 0-1
     assert(AddBooks("unexistfile.txt", &sh) == 0);
     assert(AddBooks("empty.txt", &sh) == 0);
@@ -22,26 +48,4 @@ int main()
     assert(sh.bookTypeQuantity == 3);
     assert(sh.booksQuantity == 10 + 15 + 5);
     assert(sh.totalCost == 10*1 + 15*3 + 5*10);
-    //then...
-    struct rlimit lim, softlim;
-    getrlimit(RLIMIT_AS, &softlim); 
-    //printf("\nLims:%ld-------%ld", softlim.rlim_max, softlim.rlim_cur);
-    lim.rlim_cur = 1; 
-    lim.rlim_max = sizeof(Book*) * 5; 
-    setrlimit(RLIMIT_AS, &lim); 
-    
-    //first malloc break
-    assert(AddBooks("booksfortest.txt", &sh) == 0);  
-
-    //restore..
-    setrlimit(RLIMIT_AS, &softlim);
-    assert(AddBooks("books.txt", &sh) == 1); 
-    assert(sh.books != NULL);
-    
-    lim.rlim_max = sizeof(Book*) * 4; 
-    setrlimit(RLIMIT_AS, &lim);
-    //secmalloc
-    AddBooks("books.txt", &sh); 
-    assert(sh.books == NULL);
-    printf("LOL srabotalo"); 
 }
