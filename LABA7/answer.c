91bfee466be2106c1b7050df9b31974d99237334
ZhekLu <justletmego@list.ru>
2021-05-26 17:30:22 +0300

    remake Answer.c for linux & testA

--- LABA7/answer.c
+++ LABA7/answer.c
@@ -1,15 +1,34 @@
 #include "answer.h"
 
+
+//without conio.h for linux
 int getAnswer(int from, int to)
 {
     int correctInput = 0, input = 0;
     while(!correctInput)
     {
-        input = getch(); 
-        if(input >= from + (int)'0' && input <= to + (int)'0')
-        //     printf("Uncorrect input. U should enter from %d to %d. \n", from, to);
-        // else 
+        if(scanf("%d", &input) != 1)
+        {
+            while (getchar() != '\n'){}
+            printf("\tUncorrect input. U should enter from %d to %d. \n", from, to);
+        } 
+        else if(input >= from && input <= to)
             correctInput = 1; 
     }
-    return input - '0'; 
+    return input; 
 }
+
+
+// int getAnswer(int from, int to)
+// {
+//     int correctInput = 0, input = 0;
+//     while(!correctInput)
+//     {
+//         input = getch(); 
+//         if(input >= from + (int)'0' && input <= to + (int)'0')
+//         //     printf("Uncorrect input. U should enter from %d to %d. \n", from, to);
+//         // else 
+//             correctInput = 1; 
+//     }
+//     return input - '0'; 
+// }
--- LABA7/test_answerLIB.c
+++ LABA7/test_answerLIB.c
@@ -2,7 +2,7 @@
 #include "answer.h"
 int main()
 {
-    printf("Push 0 and then 4 and 3!");  
+    printf("Push s, 0, 4 and then 3!");  
     assert(getAnswer(1, 3) == 3); 
     printf("\nNice, bro"); 
 }
